31df41a545ddd5990cfb7fdcc60c8dd937ff76fe
Shaman Narayanasamy <shaman.narayanasamy@uni.lu>
2015-12-10 21:20:05 +0100

    Edits

--- additional_analyses/iterative_assembly/MG_itr_assm_stats-v2.R
+++ additional_analyses/iterative_assembly/MG_itr_assm_stats-v2.R
@@ -9,13 +9,34 @@ library(gtable)
 library(cowplot)
 require(stringr)
 
+mytheme <- function(){
+    theme(
+        axis.text.y = element_text(size = 25),
+	axis.title.y = element_text(size = 30, vjust=1.5),
+	legend.position = c(1,1),
+	legend.justification = c(1,1),
+	legend.text = element_text(size = 30),
+	legend.title = element_text(size = 30),
+	legend.key.size = unit(1.5, "cm"),
+	legend.key = element_rect(colour = NA, fill = NA),
+	legend.background = element_rect(fill = NA, colour = "gray"),
+        axis.text.x = element_blank(),
+	axis.title.x = element_blank(),
+        axis.ticks.x = element_blank(),
+	axis.line = element_line(size = 1),
+        plot.title = element_blank(),
+        plot.margin = unit(c(0, 0, 0, 0), "lines")
+	)
+}
+
+plot_dat <- function(dat.file, pe.file, se.file){
 ### Read in assembly assessment data
-dat <- read.delim("/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/iterative_assembly/X310763260-iterative_MG/collapsed_contigs_stats.tsv")
+dat <- read.delim(dat.file)
 
 ### Read in unmappable read data
-pe <- read.table("/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/iterative_assembly/X310763260-iterative_MG/pair_counts.tsv", header=F)
+pe <- read.table(pe.file, header=F)
 colnames(pe) <- c("file", "reads_used")
-se <- read.table("/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/iterative_assembly/X310763260-iterative_MG/single_counts.tsv", header=F)
+se <- read.table(se.file, header=F)
 colnames(se) <- c("file", "reads_used")
 
 # Reformulate table
@@ -29,78 +50,103 @@ pe <-
 
 additional_mapped=c(NA, pe$mappable[2:nrow(pe)] - pe$mappable[1:nrow(pe)-1])
 pe <- cbind(pe, additional_mapped)
+pe$additional_mapped[1]  <- pe$mappable[1]
+
+
 
 se <- 
     cbind(se, 
-	  unmappable=c(se$reads_used[2:6], NA), 
 	  type=rep("SE", nrow(pe)), 
 	  Assembly=1:6, 
+	  unmappable=c(se$reads_used[2:6], NA), 
 	  mappable=c(se$reads_used[1] - se$reads_used[2:6], NA)
 )
 
 additional_mapped=c(NA, se$mappable[2:nrow(se)] - se$mappable[1:nrow(se)-1])
 se <- cbind(se, additional_mapped)
+se$additional_mapped[1]  <- se$mappable[1]
 
-reads <- rbind(pe, se)
+dat.1 <- as.data.frame(
+		       cbind(Assembly=as.character(1:6),
+			     additional_mapped=2*(pe$additional_mapped) + se$additional_mapped,
+			     genome_size=c(dat[1,c(28)], dat[2:nrow(dat),c(28)] - dat[1:nrow(dat)-1,c(28)])
+			     )
+		       )
+dat.1$additional_mapped <- as.numeric(as.character(dat.1$additional_mapped))
+dat.1$genome_size <- as.numeric(as.character(dat.1$genome_size))
 
-### Plot raw number of total contigs and genes
-dat$Assembly=as.character(1:6)
-m.dat <- melt(dat[, c(1,2,3,23)])
-colnames(m.dat)[2:3] <- c("type", "value")
+### Plot number of reads and total assembly bases
+colnames(m.dat.1) <- c("Assembly", "type", "count")
 
-contigs <- ggplot(data=m.dat, aes(x=Assembly, y=value, fill=type)) + 
+p1 <- ggplot(data=m.dat.1, aes(x=Assembly, y=log10(count), fill=type)) + 
 geom_bar(stat="identity", position="dodge") +
-ggtitle("All contigs")
+scale_fill_manual(values = c("darkorange1", "lightseagreen"), 
+		    labels = c("mappable reads", "base pairs")
+		    ) +
+guides(fill = guide_legend(title = "Additional")) +
+mytheme() 
 
 ### Plot gain of contigs and genes relative to previous assembly
-d.dat <- dat[2:nrow(dat),c(3,22)] - dat[1:nrow(dat)-1,c(3,22)]
-d.dat <- rbind(dat[1,c(3,22)], d.dat)
-d.dat <- cbind(as.character(dat[,1]), d.dat)
-colnames(d.dat)[1] <- "Assembly"
+dat.2 <- dat[2:nrow(dat),c(3,22)] - dat[1:nrow(dat)-1,c(3,22)]
+dat.2 <- rbind(dat[1,c(3,22)], dat.2)
+dat.2 <- cbind(as.character(dat[,1]), dat.2)
+colnames(dat.2)[1] <- "Assembly"
+dat.2$Assembly <- as.character(1:6)
 
-m.d.dat <- melt(d.dat)
-m.d.dat$value[m.d.dat$value==0] = NA
-colnames(m.d.dat)[2:3] <- c("type", "value")
+m.dat.2 <- melt(dat.2)
+colnames(m.dat.2)[2:3] <- c("type", "count")
+m.dat.2$count[m.dat.2$count==0] = NA
 
-contig.gain <- ggplot(data=m.d.dat, aes(x=Assembly, y=log10(value), fill=type)) + 
+p2 <- ggplot(data=m.dat.2, aes(x=Assembly, y=log10(count), fill=type)) + 
 geom_bar(stat="identity", position="dodge") +
-ggtitle("Additional contigs")
+scale_fill_manual(values = c("seagreen", "salmon1"), 
+		    labels = c("contigs > 1kb", "unique genes")
+		    ) +
+guides(fill = guide_legend(title = "Additional")) +
+mytheme() +
+theme(axis.title.x = element_text(size = 35),
+      axis.text.x = element_text(size = 30, vjust=0),
+      plot.margin = unit(c(0.5, 0.5, 1, 4) , "lines")
+      ) +
+xlab("Assembly")
 
-### Plot N50 values
-c.dat <- dat[1:nrow(dat),c(1,27,17,18)]
-m.c.dat <- melt(c.dat)
-colnames(m.c.dat)[2:3] <- c("type", "value")
+### Plot all the values in a single plot
+m.dat <- rbind(m.dat.1, m.dat.2)
 
-contig.len <- ggplot(data=m.c.dat, aes(x=Assembly, y=value, fill=type)) + 
+p3 <- ggplot(data=m.dat, aes(x=Assembly, y=log10(count), fill=type)) + 
 geom_bar(stat="identity", position="dodge") +
-ggtitle("Contiguity")
+scale_fill_manual(values = c("darkorange1", "lightseagreen", "seagreen", "salmon"), 
+		    labels = c("Mappable reads", "Base pairs", 
+			       "Volume", "Information")
+		    ) +
+guides(fill = guide_legend(title = "Additional")) +
+mytheme() +
+theme(axis.title.x = element_text(size = 35),
+      axis.text.x = element_text(size = 30, vjust=0),
+      plot.margin = unit(c(0.5, 0.5, 1, 4) , "lines")
+      ) +
+xlab("Assembly")
 
-### Plot number of unmappable reads
-mappable <- ggplot(data=reads, aes(x=Assembly, y=mappable, fill=type)) + 
-geom_bar(stat="identity", position="dodge") +
-ggtitle("Mappabe reads")
+list(p1, p2, p3)
+}
 
-unmappable <- ggplot(data=reads, aes(x=Assembly, y=unmappable, fill=type)) + 
-geom_bar(stat="identity", position="dodge") +
-ggtitle("Unmappable reads")
+HF.dat <- "/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/iterative_assembly/X310763260-iterative_MG/collapsed_contigs_stats.tsv"
+HF.pe <- "/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/iterative_assembly/X310763260-iterative_MG/pair_counts.tsv"
+HF.se <- "/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/iterative_assembly/X310763260-iterative_MG/single_counts.tsv"
 
-add.mapped <- ggplot(data=reads, aes(x=Assembly, y=log10(additional_mapped), fill=type)) + 
-geom_bar(stat="identity", position="dodge") +
-ggtitle("Additonal mapped")
+HF.plots <- plot_dat(HF.dat, HF.pe, HF.se)
 
-png("iterative_assm-draft_all.png", height=1200, width=600)
-plot_grid(contigs, contig.gain, contig.len, mappable, unmappable, add.mapped, ncol=1, align="v")
+WW.dat <- "/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/iterative_assembly/A02-iterative_MG/collapsed_contigs_stats.tsv"
+WW.pe <- "/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/iterative_assembly/A02-iterative_MG/pair_counts.tsv"
+WW.se <- "/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/iterative_assembly/A02-iterative_MG/single_counts.tsv"
+
+WW.plots <- plot_dat(WW.dat, WW.pe, WW.se)
+
+### Generate plots
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/MG_iter_assm.pdf", 
+    height=15, width=15)
+plot_grid(HF.plots[[3]] + mytheme(), 
+	  WW.plots[[3]] + guides(fill=FALSE), ncol=1, align="v", labels=c("(A)", "(B)"), label_size=45, hjust=-0.5)
 dev.off()
 
-png("iterative_assm-draft_informative.png", height=600, width=600)
-plot_grid(contig.gain, add.mapped, ncol=1, align="v")
-dev.off()
 
-### Plot as a heatmap
-#m.dat <- t(matrix(as.numeric(unlist(dat)),nrow=nrow(dat)))
-#
-#
-#heatmap(m.dat, Rowv=NA, Colv=NA, scale="column")
-#heatmap(m.dat, Rowv=NA, Colv=NA, scale="row")
-#heatmap.2(m.dat, Rowv=FALSE, Colv=FALSE, symm=FALSE, scale="column")
-#
