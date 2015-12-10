807dcda4b371808548db020e3097ac592dbd474c
Shaman Narayanasamy <shaman.narayanasamy@uni.lu>
2015-12-11 00:14:42 +0100

    Edit tables

--- additional_analyses/function_analysis/function_analysis.R
+++ additional_analyses/function_analysis/function_analysis.R
@@ -46,24 +46,19 @@ get.MT.genes <- function(MG.file, MT.file, workspace){
 }
 
 
-A02.MG <- "/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/datasets/IMP_analysis/A02_20151006-idba/Analysis/MG.gene_depth.avg"
-A02.MT <- "/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/datasets/IMP_analysis/A02_20151006-idba/Analysis/MT.gene_depth.avg"
-A02.wkspc <- "/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/datasets/IMP_analysis/A02_20151006-idba/Analysis/results/MGMT_results.Rdat"
+A02.MG <- "/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/datasets/IMP_analysis/A02_20151130-idba/Analysis/MG.gene_depth.avg"
+A02.MT <- "/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/datasets/IMP_analysis/A02_20151130-idba/Analysis/MT.gene_depth.avg"
+A02.wkspc <- "/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/datasets/IMP_analysis/A02_20151130-idba/Analysis/results/MGMT_results.Rdat"
 
-D36.MG <- "/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/datasets/IMP_analysis/D36_20151007-idba/Analysis/MG.gene_depth.avg"
-D36.MT <- "/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/datasets/IMP_analysis/D36_20151007-idba/Analysis/MT.gene_depth.avg"
-D36.wkspc <- "/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/datasets/IMP_analysis/D36_20151007-idba/Analysis/results/MGMT_results.Rdat"
+#D36.MG <- "/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/datasets/IMP_analysis/D36_20151007-idba/Analysis/MG.gene_depth.avg"
+#D36.MT <- "/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/datasets/IMP_analysis/D36_20151007-idba/Analysis/MT.gene_depth.avg"
+#D36.wkspc <- "/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/datasets/IMP_analysis/D36_20151007-idba/Analysis/results/MGMT_results.Rdat"
 
 X310763260.MG <- "/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/datasets/IMP_analysis/X310763260_20151004-idba/Analysis/MG.gene_depth.avg"
 X310763260.MT <- "/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/datasets/IMP_analysis/X310763260_20151004-idba/Analysis/MT.gene_depth.avg"
 X310763260.wkspc <- "/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/datasets/IMP_analysis/X310763260_20151004-idba/Analysis/results/MGMT_results.Rdat"
 
-
 ## Get genes only expressed in MT data sets
-dat <- get_expressed(X310763260.MG, X310763260.MT, X310763260.wkspc)
-MT_only <-  dat[dat$MG_depth==0,]
-write.table(MT_only, "/home/shaman/Documents/Publications/IMP-manuscript/tables/X310763260_MT_only.csv", row.names=F, sep="\t", quote=F)
-
 dat <- get_expressed(X310763260.MG, X310763260.MT, X310763260.wkspc)
 MT_only <-  dat[dat$MG_depth==0,]
 write.table(MT_only, "/home/shaman/Documents/Publications/IMP-manuscript/tables/X310763260_MT_only.tsv", row.names=F, sep="\t", quote=F)
@@ -72,9 +67,9 @@ dat <- get_expressed(A02.MG, A02.MT, A02.wkspc)
 MT_only <-  dat[dat$MG_depth==0,]
 write.table(MT_only, "/home/shaman/Documents/Publications/IMP-manuscript/tables/A02_MT_only.tsv", row.names=F, sep="\t", quote=F)
 
-dat <- get_expressed(D36.MG, D36.MT, D36.wkspc)
-MT_only <-  dat[dat$MG_depth==0,]
-write.table(MT_only, "/home/shaman/Documents/Publications/IMP-manuscript/tables/D36_MT_only.tsv", row.names=F, sep="\t", quote=F)
+#dat <- get_expressed(D36.MG, D36.MT, D36.wkspc)
+#MT_only <-  dat[dat$MG_depth==0,]
+#write.table(MT_only, "/home/shaman/Documents/Publications/IMP-manuscript/tables/D36_MT_only.tsv", row.names=F, sep="\t", quote=F)
 
 ## Get genes only expressed in MT data sets and on MT only contigs
 
@@ -90,9 +85,9 @@ sink("/home/shaman/Documents/Publications/IMP-manuscript/tables/A02_MT_contigs.t
 print(xtable(dat.MT[,c(1:3)]), floating=F, include.rownames=F)
 sink()
 
-dat <- get.MT.genes(D36.MG, D36.MT, D36.wkspc)
-dat.MT <- dat[is.na(dat$MG_contig_depth) & dat$contig_len >= 500, c(5,1,2,4,6,8,9)]
-sink("/home/shaman/Documents/Publications/IMP-manuscript/tables/D36_MT_contigs.tex")
-print(xtable(dat.MT[,c(1:3)]), floating=F, include.rownames=F)
-sink()
+#dat <- get.MT.genes(D36.MG, D36.MT, D36.wkspc)
+#dat.MT <- dat[is.na(dat$MG_contig_depth) & dat$contig_len >= 500, c(5,1,2,4,6,8,9)]
+#sink("/home/shaman/Documents/Publications/IMP-manuscript/tables/D36_MT_contigs.tex")
+#print(xtable(dat.MT[,c(1:3)]), floating=F, include.rownames=F)
+#sink()
 
