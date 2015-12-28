60b9a3b4b13f69952b034dd621996747e2e04032
Shaman Narayanasamy <shaman.narayanasamy@uni.lu>
2015-12-28 15:14:46 +0100

    Update figure labels

--- additional_analyses/comparison/comparison.R
+++ additional_analyses/comparison/comparison.R
@@ -97,10 +97,10 @@ dat2 <- read.delim("/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis
 ## Read in WW vs Bio17-1
 dat3 <- read.delim("/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/quast_output/quast_A02_Microthrix_Bio17-20151208/combined_reference/transposed_report.tsv", header=T)
 
-png("/home/shaman/Documents/Publications/IMP-manuscript/figures/comparison_all-v5.png", 
+png("/home/shaman/Documents/Publications/IMP-manuscript/figures/comparison_all-v6.png", 
     width=2900, height=900)
 par(mfrow=c(1,3), mar = c(10,0,0,0))
-plot.basic.stats(dat1, cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd, title = "(A) Mock") 
+plot.basic.stats(dat1, cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd, title = "(A) SM") 
 plot.basic.stats(dat2, cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd, title = "(B) HF") 
 plot.basic.stats(dat3, cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd, title = "(C) WW") 
 
