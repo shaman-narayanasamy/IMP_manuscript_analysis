272e81b72ded6852f9ce9f84ef9ac464213096a9
Shaman Narayanasamy <shaman.narayanasamy@uni.lu>
2015-12-29 14:59:02 +0100

    New quast commands and new script for simulating MG reads

---
+++ additional_analyses/simulated_data/NeSSM_simulate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+
+SCRIPTS="/home/users/snarayanasamy/NeSSM/scripts"
+BIN="/home/users/snarayanasamy/NeSSM/NeSSM_CPU/NeSSM"
+
+perl $SCRIPTS/mk_index.pl /mnt/nfs/projects/ecosystem_biology/test_datasets/CelajEtAl/73_species /mnt/nfs/projects/ecosystem_biology/test_datasets/CelajEtAl/indexes
+mv /mnt/nfs/projects/ecosystem_biology/test_datasets/CelajEtAl/indexes/index /mnt/nfs/projects/ecosystem_biology/test_datasets/CelajEtAl/indexes/index_back
+
+paste <(cut -f5 /mnt/nfs/projects/ecosystem_biology/test_datasets/CelajEtAl/indexes/index_back | cut -f9 -d "/") <(cat /mnt/nfs/projects/ecosystem_biology/test_datasets/CelajEtAl/indexes/index_back) > /mnt/nfs/projects/ecosystem_biology/test_datasets/CelajEtAl/indexes/index
+paste <(cut -f1 /mnt/nfs/projects/ecosystem_biology/test_datasets/CelajEtAl/indexes/index) <(cut -f2 /mnt/nfs/projects/ecosystem_biology/test_datasets/CelajEtAl/73_species_abundance.txt) -d "\t" > /mnt/nfs/projects/ecosystem_biology/test_datasets/CelajEtAl/73_species_abundance-new.txt
+
+perl $SCRIPTS/adjust.pl /mnt/nfs/projects/ecosystem_biology/test_datasets/CelajEtAl/73_species_abundance-new.txt indexes/index
+
+$BIN -list  /mnt/nfs/projects/ecosystem_biology/test_datasets/CelajEtAl/new-percentage.txt -index  /mnt/nfs/projects/ecosystem_biology/test_datasets/CelajEtAl/indexes/index -m illumina -o 73_species_MG -r 10000000 -l 120 -e 1 -c /home/users/snarayanasamy/NeSSM/NeSSM_CPU/simulation.config
+
