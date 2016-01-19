295eb8217ba4ab08401f7e26b1f3db16394cef34
Shaman Narayanasamy <shaman.narayanasamy@uni.lu>
2016-01-19 15:19:35 +0100

    Comment out commands

--- additional_analyses/MT_genomes/MT_genomes.sh
+++ additional_analyses/MT_genomes/MT_genomes.sh
@@ -8,13 +8,16 @@ DIR_WW=/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/A02_20151004-idba-v
 INPUT_HF=/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/X310763260_20151004-idba/Assembly/MGMT.assembly.merged.fa
 INPUT_WW=/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/A02_20151130-idba/Assembly/MGMT.assembly.merged.fa
 
-## Extract contig IDs from the tsv file
-cut -f1 X310763260_MT_contigs.tsv | tail -n +2 | uniq > X310763260_MT_contigs.ids
-cut -f1 A02_MT_contigs.tsv | tail -n +2 | uniq > A02_MT_contigs.ids
+### These steps were done on my local machine (commented out)
+## Extract contig IDs from the tsv file.
+
+#cut -f1 X310763260_MT_contigs.tsv | tail -n +2 | uniq > X310763260_MT_contigs.ids
+#cut -f1 A02_MT_contigs.tsv | tail -n +2 | uniq > A02_MT_contigs.ids
 
 ## Copy the contig IDs onto gaia
-scp X310763260_MT_contigs.ids gaia:/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/X310763260_20151004-idba-virusContigs
-scp A02_MT_contigs.ids gaia:/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/A02_20151004-idba-virusContigs
+
+#scp X310763260_MT_contigs.ids gaia:/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/X310763260_20151004-idba-virusContigs
+#scp A02_MT_contigs.ids gaia:/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/A02_20151004-idba-virusContigs
 
 # Extract the sequences
 pullseq -i $INPUT_HF -n $DIR_HF/X310763260_MT_contigs.ids 
