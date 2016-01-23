00010ee237872aa1756a80e85d81ebe26bf1e51e
Shaman Narayanasamy <shaman.narayanasamy@uni.lu>
2016-01-23 18:27:35 +0100

    Include same omic mappings

--- additional_analyses/comparison/mappingCommands.sh
+++ additional_analyses/comparison/mappingCommands.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
-# Map SM MG to MT-only assembly
+## Map SM MG to MT-only assembly
+#./readMappingSingleOmics.sh \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/SM_IMP/Preprocessing/MG.R1.preprocessed.fq \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/SM_IMP/Preprocessing/MG.R2.preprocessed.fq \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/SM_IMP/Preprocessing/MG.SE.preprocessed.fq \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/simDat-iterative_MT/MT_contigs_merged_2.fa \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/mappingSingleOmics/SM \
+#	MG \
+#	MT \
+#	SM
+#
+## Map SM MT to MT-only assembly
+#./readMappingSingleOmics.sh \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/SM_IMP/Preprocessing/MT.R1.preprocessed.fq \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/SM_IMP/Preprocessing/MT.R2.preprocessed.fq \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/SM_IMP/Preprocessing/MT.SE.preprocessed.fq \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/simDat-iterative_MG/MG_contigs_merged_2.fa \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/mappingSingleOmics/SM \
+#	MT \
+#	MG \
+#	SM
+#
+# Map SM MG to MG-only assembly
 ./readMappingSingleOmics.sh \
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/SM_IMP/Preprocessing/MG.R1.preprocessed.fq \
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/SM_IMP/Preprocessing/MG.R2.preprocessed.fq \
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/SM_IMP/Preprocessing/MG.SE.preprocessed.fq \
-	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/simDat-iterative_MT/MT_contigs_merged_2.fa \
+	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/simDat-iterative_MG/MG_contigs_merged_2.fa \
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/mappingSingleOmics/SM \
 	MG \
-	MT \
+	MG \
 	SM
 
 # Map SM MT to MT-only assembly
@@ -16,21 +38,44 @@
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/SM_IMP/Preprocessing/MT.R1.preprocessed.fq \
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/SM_IMP/Preprocessing/MT.R2.preprocessed.fq \
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/SM_IMP/Preprocessing/MT.SE.preprocessed.fq \
-	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/simDat-iterative_MG/MG_contigs_merged_2.fa \
+	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/simDat-iterative_MT/MT_contigs_merged_2.fa \
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/mappingSingleOmics/SM \
 	MT \
-	MG \
+	MT \
 	SM
 
-# Map HF MG to MT-only assembly
+## Map HF MG to MT-only assembly
+#./readMappingSingleOmics.sh \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/HF_IMP/Preprocessing/MG.R1.preprocessed.fq \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/HF_IMP/Preprocessing/MG.R2.preprocessed.fq \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/HF_IMP/Preprocessing/MG.SE.preprocessed.fq \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/simDat-iterative_MT/MT_contigs_merged_2.fa \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/mappingSingleOmics/HF \
+#	MG \
+#	MT \
+#	HF
+#
+## Map HF MT to MT-only assembly
+#./readMappingSingleOmics.sh \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/HF_IMP/Preprocessing/MT.R1.preprocessed.fq \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/HF_IMP/Preprocessing/MT.R2.preprocessed.fq \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/HF_IMP/Preprocessing/MT.SE.preprocessed.fq \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/simDat-iterative_MG/MG_contigs_merged_2.fa \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/mappingSingleOmics/HF \
+#	MT \
+#	MG \
+#	HF
+#
+
+# Map HF MG to MG-only assembly
 ./readMappingSingleOmics.sh \
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/HF_IMP/Preprocessing/MG.R1.preprocessed.fq \
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/HF_IMP/Preprocessing/MG.R2.preprocessed.fq \
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/HF_IMP/Preprocessing/MG.SE.preprocessed.fq \
-	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/simDat-iterative_MT/MT_contigs_merged_2.fa \
+	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/simDat-iterative_MG/MG_contigs_merged_2.fa \
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/mappingSingleOmics/HF \
 	MG \
-	MT \
+	MG \
 	HF
 
 # Map HF MT to MT-only assembly
@@ -38,21 +83,44 @@
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/HF_IMP/Preprocessing/MT.R1.preprocessed.fq \
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/HF_IMP/Preprocessing/MT.R2.preprocessed.fq \
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/HF_IMP/Preprocessing/MT.SE.preprocessed.fq \
-	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/simDat-iterative_MG/MG_contigs_merged_2.fa \
+	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/simDat-iterative_MT/MT_contigs_merged_2.fa \
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/mappingSingleOmics/HF \
 	MT \
-	MG \
+	MT \
 	HF
 
-# Map WW MG to MT-only assembly
+## Map WW MG to MT-only assembly
+#./readMappingSingleOmics.sh \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/WW_IMP/Preprocessing/MG.R1.preprocessed.fq \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/WW_IMP/Preprocessing/MG.R2.preprocessed.fq \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/WW_IMP/Preprocessing/MG.SE.preprocessed.fq \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/simDat-iterative_MT/MT_contigs_merged_2.fa \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/mappingSingleOmics/WW \
+#	MG \
+#	MT \
+#	WW
+#
+## Map WW MT to MT-only assembly
+#./readMappingSingleOmics.sh \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/WW_IMP/Preprocessing/MT.R1.preprocessed.fq \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/WW_IMP/Preprocessing/MT.R2.preprocessed.fq \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/WW_IMP/Preprocessing/MT.SE.preprocessed.fq \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/simDat-iterative_MG/MG_contigs_merged_2.fa \
+#	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/mappingSingleOmics/WW \
+#	MT \
+#	MG \
+#	WW
+#
+
+# Map WW MG to MG-only assembly
 ./readMappingSingleOmics.sh \
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/WW_IMP/Preprocessing/MG.R1.preprocessed.fq \
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/WW_IMP/Preprocessing/MG.R2.preprocessed.fq \
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/WW_IMP/Preprocessing/MG.SE.preprocessed.fq \
-	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/simDat-iterative_MT/MT_contigs_merged_2.fa \
+	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/simDat-iterative_MG/MG_contigs_merged_2.fa \
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/mappingSingleOmics/WW \
 	MG \
-	MT \
+	MG \
 	WW
 
 # Map WW MT to MT-only assembly
@@ -60,9 +128,10 @@
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/WW_IMP/Preprocessing/MT.R1.preprocessed.fq \
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/WW_IMP/Preprocessing/MT.R2.preprocessed.fq \
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/WW_IMP/Preprocessing/MT.SE.preprocessed.fq \
-	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/simDat-iterative_MG/MG_contigs_merged_2.fa \
+	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/simDat-iterative_MT/MT_contigs_merged_2.fa \
 	/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/mappingSingleOmics/WW \
 	MT \
-	MG \
+	MT \
 	WW
+
 
