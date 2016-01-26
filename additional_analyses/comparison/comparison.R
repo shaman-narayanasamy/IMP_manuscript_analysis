0f4ddcf8f8a49817bd7bd56fab580351176b2993
Shaman Narayanasamy <shaman.narayanasamy@uni.lu>
2016-01-26 17:55:09 +0100

    Major refactor of radar chart figure

--- additional_analyses/comparison/comparison.R
+++ additional_analyses/comparison/comparison.R
@@ -29,19 +29,19 @@ makeTransparent = function(..., alpha=0.5) {
 
 }
 
-######################################################################################################
-## Read assessment of simulated dat
-
-plot.basic.stats <- function(dat, cols, dens, font, linetype, 
-			     fsize, linewd, mcex, lwd, plwd, title = "") 
-{
+###############################################################################################################################
+### FUNCTION: 5-axis radar chart
+plot.5axis <- function(dat, cols, dens, font, linetype, 
+			     fsize, linewd, mcex, lwd, plwd, title = "") {
 dat <- dat[,c("Assembly", 
        "X..contigs.....1000.bp.", 
        "N50", 
        "X..predicted.genes..unique.",
+       "X..misassemblies",
        "Genome.fraction...."
        )]
-colnames(dat) <- c("Assembly", "contigs \u2265 1kb", "N50 length", "no. of genes", "genome fraction (%)")
+
+colnames(dat) = c("Assembly", "contigs \u2265 1kb", "N50 length", "no. of genes", "misassemblies", "genome fraction (%)")
 
 dat$Assembly <- factor(dat$Assembly, levels=c(levels(dat$Assembly), 'max', 'min'))
 dat <- rbind(
@@ -57,15 +57,9 @@ dat[1, !names(dat) %in% c("Assembly")] <-
 dat[2, !names(dat) %in% c("Assembly")] <- 
     dat[2, !names(dat) %in% c("Assembly")] * 0.75
 
-# Calculate maximum and minimum for negative values
-#dat[1, names(dat) %in% c("Accuracy")] <- 
-#    round(dat[1, names(dat) %in% c("Accuracy")] * 0.75)
-#dat[2, names(dat) %in% c("Accuracy")] <- 
-#    round(dat[2, names(dat) %in% c("Accuracy")] * 1.025)
-
 # Set min and max for percentages
-dat[1, names(dat) %in% c("Recovery")] <- 100
-dat[2, names(dat) %in% c("Recovery")] <- 0
+dat[1, names(dat) %in% c("Genome.fraction....")] <- 100
+dat[2, names(dat) %in% c("Genome.fraction....")] <- 0
 
 par(mar=c(0,0,8,0)) 
 radarchart(dat[,-1], pcol=cols, pfcol=dens, plty=linetype, plwd=plwd,
@@ -75,6 +69,58 @@ radarchart(dat[,-1], pcol=cols, pfcol=dens, plty=linetype, plwd=plwd,
 title(title, outer = FALSE, line = 2, font = 2, cex.main = 7.5)
 }
 
+###############################################################################################################################
+### FUNCTION: 3-axis radar chart
+plot.3axis <- function(dat, cols, dens, font, linetype, 
+			     fsize, linewd, mcex, lwd, plwd, title = "") {
+dat <- dat[,c("Assembly", 
+       "X..contigs.....1000.bp.", 
+       "N50", 
+       "X..predicted.genes..unique."
+       )]
+
+colnames(dat) = c("Assembly", "contigs \u2265 1kb", "N50 length", "no. of genes")
+
+dat$Assembly <- factor(dat$Assembly, levels=c(levels(dat$Assembly), 'max', 'min'))
+dat <- rbind(
+	     c("max", apply(dat[, 2:ncol(dat)], 2, max)), 
+	     c("min", apply(dat[, 2:ncol(dat)], 2, min)), 
+	     dat)
+
+dat[,2:ncol(dat)] <- sapply(dat[,2:ncol(dat)], as.numeric)
+	     
+# Calculate maximum and minimum for positive values
+dat[1, !names(dat) %in% c("Assembly")] <- 
+    dat[1, !names(dat) %in% c("Assembly")] * 1.025
+dat[2, !names(dat) %in% c("Assembly")] <- 
+    dat[2, !names(dat) %in% c("Assembly")] * 0.75
+
+par(mar=c(0,0,8,0)) 
+radarchart(dat[,-1], pcol=cols, pfcol=dens, plty=linetype, plwd=plwd,
+	   cglwd=lwd, palcex=font, calcex=font, vlcex=font, cglcol="darkgray",
+	   paxislabels = c("contigs \u2265 1kb", "N50 length", "no. of genes"),
+	   oma=c(0,0,0,0))#, title = title, cex.main = 6)
+title(title, outer = FALSE, line = 2, font = 2, cex.main = 7.5)
+}
+
+
+### Produce radar chart for the basic assembly statistics
+## Read in simulated data
+dat1 <- read.delim("/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/quast_output/quast_simDat_20151028/combined_reference/transposed_report.tsv", header=T)
+
+## Read in HF
+dat2 <- read.delim("/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/quast_output/quast_X310763260-20151028/combined_reference/transposed_report.tsv", header=T)
+
+## Read in WW vs Bio17-1
+dat3 <- read.delim("/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/quast_output/quast_A02_Microthrix_Bio17-20151208/combined_reference/transposed_report.tsv", header=T)
+
+## Read in sum of all metrices
+dat4 <- read.delim("/home/shaman/Documents/Publications/IMP-manuscript/tables/summary_sum.csv", header=T)
+
+## Read in mean of all metrices
+dat5 <- read.delim("/home/shaman/Documents/Publications/IMP-manuscript/tables/summary_mean.csv", header=T)
+
+## Set plot values
 cols <- makeTransparent("darkred", "darkblue", "darkgreen", "darkorange2", alpha=0.75)
 dens <- makeTransparent("red", "blue", "green", "orange", alpha=0.10)
 font=10
@@ -85,61 +131,41 @@ mcex=8
 lwd=16
 plwd=15
 
-## Produce radar chart for the basic assembly statistics
-
-## Read in simulated data
-dat1 <- read.delim("/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/quast_output/quast_simDat_20151028/combined_reference/transposed_report.tsv", header=T)
-
-## Read in HF
-dat2 <- read.delim("/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/quast_output/quast_X310763260-20151028/combined_reference/transposed_report.tsv", header=T)
-
-## Read in WW vs Bio17-1
-dat3 <- read.delim("/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/quast_output/quast_A02_Microthrix_Bio17-20151208/combined_reference/transposed_report.tsv", header=T)
-
-png("/home/shaman/Documents/Publications/IMP-manuscript/figures/comparison_all-v7.png", 
-    width=2900, height=1500)
-
-pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/comparison_all-v8.pdf", 
-    width=30, height=15)
-par(mfrow=c(1,3), mar = c(10,0,0,0))
-plot.basic.stats(dat1, cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd, title = "(A) SM") 
-plot.basic.stats(dat2, cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd, title = "(B) HF") 
-plot.basic.stats(dat3, cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd, title = "(C) WW") 
-
-par(fig = c(0, 1, 0, 1), oma = c(0.5, 0, 0, 0), mar = c(0, 0, 0, 0), new = TRUE)
-plot(0, 0, type = "n", bty = "n", xaxt = "n", yaxt = "n")
-legend("bottom", legend=dat1[,1], xpd = TRUE, horiz = TRUE, inset = c(0, 
-    0), bty = "o", col = cols, lty=linetype, lwd = c(4,4,4,4), cex = 4,
-       text.font=c(4,4,4,4), box.lty=1, box.lwd=0, box.col="gray",
-       text.col=cols)
-
-dev.off()
-
-
-pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/SM_radarChart_v2.pdf", 
+## Start producing plots
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/SM_radarChart_v3.pdf", 
     width=38, height=25)
-plot.basic.stats(dat1, cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
+plot.5axis(dat1, cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
 dev.off()
 
-pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/HF_radarChart_v2.pdf", 
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/HF_radarChart_v3.pdf", 
     width=38, height=25)
-plot.basic.stats(dat2, cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
+plot.3axis(dat2, cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
 dev.off()
 
-pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/WW_radarChart_v2.pdf", 
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/WW_radarChart_v3.pdf", 
     width=38, height=25)
-plot.basic.stats(dat3, cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
+plot.3axis(dat3, cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
 dev.off()
 
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/summaryRadarChartSum.pdf", 
+    width=38, height=25)
+plot.5axis(dat4, cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
+dev.off()
+
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/summaryRadarChartMean.pdf", 
+    width=38, height=25)
+plot.5axis(dat5, cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
+dev.off()
+
+## Produce legend
 pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/radarChart_legend.pdf", 
     width=40, height=2.5)
 par(fig = c(0, 1, 0, 1), oma = c(0.5, 0, 0, 0), mar = c(0, 0, 0, 0), new = TRUE)
 plot(0, 0, type = "n", bty = "n", xaxt = "n", yaxt = "n")
-legend("bottom", legend=dat1[,1], xpd = TRUE, horiz = TRUE, inset = c(0, 
-    0), bty = "o", col = cols, lty=linetype, lwd = c(4,4,4,4), cex = 4,
+legend("bottom", legend=c("IMP", "IMP-MEGAHIT", "MetAMOS", "MetAMOS-IDBA_UD"), xpd = TRUE, horiz = TRUE, inset = c(0, 
+    0), bty = "o", col = cols, lty=linetype, lwd = c(15,15,15,15), cex = 4,
        text.font=c(4,4,4,4), box.lty=1, box.lwd=0, box.col="gray",
        text.col=cols)
 
 dev.off()
-
 
