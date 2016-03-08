e4bbb3668fdea6dbb583f3e3e1ab4f517dc00beb
Shaman Narayanasamy <shaman.narayanasamy@uni.lu>
2016-03-08 12:49:37 +0100

    Dry run success

--- additional_analyses/HMP_gene_catalog/execution.sh
+++ additional_analyses/HMP_gene_catalog/execution.sh
@@ -3,15 +3,21 @@
 source preload_modules.sh
 
 date
+MGR1=$1
+MGR2=$2
+MTR1=$3
+MTR2=$4
+SAMPLE=$5
+OUT_DIR=$6
+OUT_LOG=$6/${5}_IGC.log
 
-CMD="MGR1=$1 \
-  MGR2=$2 \
-  MTR1=$3 \
-  MTR2=$4 \
-  SAMPLE=$5 \
-  OUT_DIR=$6 \
-  OUT_LOG=$OUT_DIR/$SAMPLE.log \
-  snakemake ALL -np"
-echo $CMD
+MGR1=$MGR1 \
+  MGR2=$MGR2 \
+  MTR1=$MTR1 \
+  MTR2=$MTR2 \
+  SAMPLE=$SAMPLE \
+  OUT_DIR=$OUT_DIR \
+  OUT_LOG=$OUT_LOG \
+  snakemake ALL -np
 
 date
