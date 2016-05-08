844b8d4002b4fc3431960af69f0a6de88cd567dd
Shaman Narayanasamy <shaman.narayanasamy@uni.lu>
2016-05-08 16:50:50 +0200

    Re-add comparison scripts

---
+++ additional_analyses/comparison/comparison.R
@@ -0,0 +1,184 @@
+#!/bin/R
+
+require(fmsb)
+require(ggplot2)
+require(grDevices)
+library(dplyr)
+library(plyr)
+library(tidyr)
+library(graphics)
+library(cowplot)
+
+######################################################################################################
+##### Initialize functions
+######################################################################################################
+### FUNCTION: Create colours with transparency ###
+makeTransparent = function(..., alpha=0.5) {
+
+  if(alpha<0 | alpha>1) stop("alpha must be between 0 and 1")
+
+  alpha = floor(255*alpha)  
+  newColor = col2rgb(col=unlist(list(...)), alpha=FALSE)
+
+  .makeTransparent = function(col, alpha) {
+    rgb(red=col[1], green=col[2], blue=col[3], alpha=alpha, maxColorValue=255)
+  }
+
+  newColor = apply(newColor, 2, .makeTransparent, alpha=alpha)
+
+  return(newColor)
+
+}
+
+###############################################################################################################################
+### FUNCTION: 5-axis radar chart
+plot.5axis <- function(dat, cols, dens, font, linetype, 
+			     fsize, linewd, mcex, lwd, plwd, title = "") {
+dat <- dat[,c("Assembly", 
+       "X..contigs.....1000.bp.", 
+       "N50", 
+       "X..predicted.genes..unique.",
+       "X..misassemblies",
+       "Genome.fraction...."
+       )]
+
+colnames(dat) = c("Assembly", "contigs \u2265 1kb", "N50 length", "no. of genes", "misassemblies", "genome fraction (%)")
+
+dat$Assembly <- factor(dat$Assembly, levels=c(levels(dat$Assembly), 'max', 'min'))
+dat <- rbind(
+	     c("max", apply(dat[, 2:ncol(dat)], 2, max)), 
+	     c("min", apply(dat[, 2:ncol(dat)], 2, min)), 
+	     dat)
+
+dat[,2:ncol(dat)] <- sapply(dat[,2:ncol(dat)], as.numeric)
+	     
+# Calculate maximum and minimum for positive values
+dat[1, !names(dat) %in% c("Assembly")] <- 
+    dat[1, !names(dat) %in% c("Assembly")] * 1.025
+dat[2, !names(dat) %in% c("Assembly")] <- 
+    dat[2, !names(dat) %in% c("Assembly")] * 0.75
+
+# Set min and max for percentages
+dat[1, names(dat) %in% c("Genome.fraction....")] <- 100
+dat[2, names(dat) %in% c("Genome.fraction....")] <- 0
+
+par(mar=c(0,0,8,0)) 
+radarchart(dat[,-1], pcol=cols, pfcol=dens, plty=linetype, plwd=plwd,
+	   cglwd=lwd, palcex=font, calcex=font, vlcex=font, cglcol="darkgray",
+	   paxislabels = c("contigs \u2265 1kb", "N50 length", "no. of genes", "genome fraction (%)"),
+	   oma=c(0,0,0,0))#, title = title, cex.main = 6)
+title(title, outer = FALSE, line = 2, font = 2, cex.main = 7.5)
+}
+
+###############################################################################################################################
+### FUNCTION: 3-axis radar chart
+plot.3axis <- function(dat, cols, dens, font, linetype, 
+			     fsize, linewd, mcex, lwd, plwd, title = "") {
+dat <- dat[,c("Assembly", 
+       "X..contigs.....1000.bp.", 
+       "N50", 
+       "X..predicted.genes..unique."
+       )]
+
+colnames(dat) = c("Assembly", "contigs \u2265 1kb", "N50 length", "no. of genes")
+
+dat$Assembly <- factor(dat$Assembly, levels=c(levels(dat$Assembly), 'max', 'min'))
+dat <- rbind(
+	     c("max", apply(dat[, 2:ncol(dat)], 2, max)), 
+	     c("min", apply(dat[, 2:ncol(dat)], 2, min)), 
+	     dat)
+
+dat[,2:ncol(dat)] <- sapply(dat[,2:ncol(dat)], as.numeric)
+	     
+# Calculate maximum and minimum for positive values
+dat[1, !names(dat) %in% c("Assembly")] <- 
+    dat[1, !names(dat) %in% c("Assembly")] * 1.025
+dat[2, !names(dat) %in% c("Assembly")] <- 
+    dat[2, !names(dat) %in% c("Assembly")] * 0.75
+
+par(mar=c(0,0,8,0)) 
+radarchart(dat[,-1], pcol=cols, pfcol=dens, plty=linetype, plwd=plwd,
+	   cglwd=lwd, palcex=font, calcex=font, vlcex=font, cglcol="darkgray",
+	   paxislabels = c("contigs \u2265 1kb", "N50 length", "no. of genes"),
+	   oma=c(0,0,0,0))#, title = title, cex.main = 6)
+title(title, outer = FALSE, line = 2, font = 2, cex.main = 7.5)
+}
+
+
+### Produce radar chart for the basic assembly statistics
+## Read in simulated data
+dat1 <- read.delim("/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/quast_output/quast_simDat_20151028/combined_reference/transposed_report.tsv", header=T)
+
+## Read in HF
+dat2 <- read.delim("/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/quast_output/quast_X310763260-20151028/combined_reference/transposed_report.tsv", header=T)
+
+## Read in WW vs Bio17-1
+dat3 <- read.delim("/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/quast_output/quast_A02_Microthrix_Bio17-20151208/combined_reference/transposed_report.tsv", header=T)
+
+## Read in sum of all metrices
+dat4 <- read.delim("/home/shaman/Documents/Publications/IMP-manuscript/tables/summary_sum.csv", header=T)
+
+## Read in mean of all metrices
+dat5 <- read.delim("/home/shaman/Documents/Publications/IMP-manuscript/tables/summary_mean.csv", header=T)
+
+## Set plot values
+cols <- makeTransparent("darkred", "darkblue", "darkgreen", "darkorange2", alpha=0.75)
+dens <- makeTransparent("red", "blue", "green", "orange", alpha=0.10)
+font=10
+linetype <- c(1,1,2,2)
+fsize=10
+linewd=15
+mcex=8
+lwd=16
+plwd=15
+
+## Start producing plots
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/SM_radarChart_v3.pdf", 
+    width=38, height=25)
+plot.5axis(dat1, cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
+dev.off()
+
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/HF_radarChart_v3.pdf", 
+    width=38, height=25)
+plot.3axis(dat2, cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
+dev.off()
+
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/WW_radarChart_v3.pdf", 
+    width=38, height=25)
+plot.3axis(dat3, cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
+dev.off()
+
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/summaryRadarChartSum.pdf", 
+    width=38, height=25)
+plot.5axis(dat4, cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
+dev.off()
+
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/summaryRadarChartMean.pdf", 
+    width=38, height=25)
+plot.5axis(dat5, cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
+dev.off()
+
+## Produce legend
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/radarChart_legend.pdf", 
+    width=40, height=2.5)
+par(fig = c(0, 1, 0, 1), oma = c(0.5, 0, 0, 0), mar = c(0, 0, 0, 0), new = TRUE)
+plot(0, 0, type = "n", bty = "n", xaxt = "n", yaxt = "n")
+legend("bottom", legend=c("IMP", "IMP-MEGAHIT", "MetAMOS", "MetAMOS-IDBA_UD"), xpd = TRUE, horiz = TRUE, inset = c(0, 
+    0), bty = "o", col = cols, lty=linetype, lwd = c(15,15,15,15), cex = 4,
+       text.font=c(4,4,4,4), box.lty=1, box.lwd=0, box.col="gray",
+       text.col=cols)
+
+dev.off()
+
+### Create a full table for the data
+
+all.dat <- rbind.fill( 
+		      cbind(data=rep("SM",4), dat1),
+		      cbind(data=rep("HF",4), dat2),
+		      cbind(data=rep("WW",4), dat3),
+		      cbind(data=rep("ALL",4), dat4)
+)
+
+
+write.table(as.data.frame(t(all.dat)), "/home/shaman/Documents/Publications/IMP-manuscript/tables/metaQUAST_pipeline_comparison.tsv",  row.names=rownames(t(all.dat)), quote=F, sep = "\t")
+
---
+++ additional_analyses/comparison/comparison_detailed.R
@@ -0,0 +1,216 @@
+#!/bin/R
+
+require(fmsb)
+require(ggplot2)
+require(grDevices)
+library(dplyr)
+library(tidyr)
+library(graphics)
+
+######################################################################################################
+##### Initialize functions
+######################################################################################################
+### FUNCTION: Create colours with transparency ###
+makeTransparent = function(..., alpha=0.5) {
+
+  if(alpha<0 | alpha>1) stop("alpha must be between 0 and 1")
+
+  alpha = floor(255*alpha)  
+  newColor = col2rgb(col=unlist(list(...)), alpha=FALSE)
+
+  .makeTransparent = function(col, alpha) {
+    rgb(red=col[1], green=col[2], blue=col[3], alpha=alpha, maxColorValue=255)
+  }
+
+  newColor = apply(newColor, 2, .makeTransparent, alpha=alpha)
+
+  return(newColor)
+
+}
+
+### FUNCTION: Plot basic statistics ###
+plot_basic_stats <- function(dat, title=NULL, font=1, lwd=1, plwd=c(2,2,2)) {
+## Process basic satistics
+data <- dat[,c(
+	      "Assembly",
+	      "X..contigs.....1000.bp.",
+	      "N50",
+	      "Total.length",
+	      "Largest.contig",
+	      "N75",
+	      "Genome.fraction...."
+	      )]
+data$Assembly <- as.character(data$Assembly)
+
+# Convert some values into negative values (to flip the axis)
+data <- rbind(c("max", apply(data[, 2:ncol(data)], 2, max)), 
+		     c("min", apply(data[, 2:ncol(data)], 2, min)), 
+		     data)
+
+# Change data to numeric
+data[,2:ncol(data)] <- sapply(data[,2:ncol(data)], as.numeric)
+
+# Calculate maximum and minimum for positive values
+data[1, !names(data) %in% c("Assembly", "L50", "L75")] <- 
+    data[1, !names(data) %in% c("Assembly", "L50", "L75")] * 1.1
+data[2, !names(data) %in% c("Assembly", "L50", "L75")] <- 
+    data[2, !names(data) %in% c("Assembly", "L50", "L75")] * 0.9
+
+# Calculate maximum and minimum for positive values. Genome fraction set to 0 min and 100 max
+data[1, "Genome.fraction...."] <- 100
+data[2, "Genome.fraction...."] <- 0
+
+axisnames <- c("contigs > 1kbp", 
+	       "N50", 
+	       "total length", 
+	       "largest contig",
+	       "N75",
+	       "genome\nfraction"
+	       )
+
+# Produce radar chart for the basic assembly statistics
+radarchart(data[,-1], pcol=cols, pfcol=dens, plty=linetype, plwd=plwd,
+	   cglwd=lwd, palcex=font, calcex=font, vlcex=font, cglcol="gray",
+	   vlabels=axisnames, title=title, oma=c(0,0,0,0))
+}
+
+### FUNCTION: Plot misassembly statistics ###
+plot_misassembly_stats <- function(dat, title=NULL, font=1, lwd=1, plwd=c(2,2,2)) {
+## Process misassembly statistics
+data <- dat[, c("Assembly", 
+		"X..misassemblies", 
+		"Duplication.ratio", 
+		"X..mismatches.per.100.kbp",
+		"X..indels.per.100.kbp"
+		)]
+
+data$Assembly <- as.character(data$Assembly)
+
+#data[ , !names(data) %in% c("Assembly", "Genome.fraction....")] <- 
+#    -data[ , !names(data) %in% c("Assembly", "Genome.fraction....")] 
+
+data <- rbind(c("max", apply(data[,2:ncol(data)],2,max)), 
+		     c("min", apply(data[,2:ncol(data)],2,min)), 
+		     data)
+data[,2:ncol(data)] <- sapply(data[,2:ncol(data)], as.numeric)
+
+
+# Calculate maximum and minimum for positive values
+#data[1, !names(data) %in% c("Assembly", "Genome.fraction....")] <- 
+#    data[1, !names(data) %in% c("Assembly", "Genome.fraction....")] * 0.9
+#data[2, !names(data) %in% c("Assembly", "Genome.fraction....")] <- 
+#    data[2, !names(data) %in% c("Assembly", "Genome.fraction....")] * 1.1
+
+data[1, !names(data) %in% c("Assembly")] <- 
+    data[1, !names(data) %in% c("Assembly")] * 1.1
+data[2, !names(data) %in% c("Assembly")] <- 
+    data[2, !names(data) %in% c("Assembly")] * 0.9
+
+
+# Set duplication ratio max to 0 and min to -2
+data[1, "Duplication.ratio"] <- 0
+data[2, "Duplication.ratio"] <- 2
+
+axisnames <- c("misassemblies", 
+	       "duplication\nratio",
+	       "mismatches per 100 kbp",
+	       "indels\nper\n100kbp")
+
+# Produce radar chart for the basic assembly statistics
+radarchart(data[,-1], pcol=cols, pfcol=dens, plty=linetype, plwd=plwd,
+	   cglwd=lwd, palcex=font, calcex=font, vlcex=font, cglcol="gray",
+	   vlabels=axisnames,title=title, oma=c(0,0,0,0))
+}
+
+### FUNCTION: Plot gene statistics ###
+plot_gene_stats <- function(dat, title=NULL, font=1, lwd=1, plwd=c(2,2,2)) {
+## Process gene statistics
+data <- 
+    dat[, c(
+	    "Assembly", 
+	    "X..predicted.genes..unique.",
+	    "X..predicted.genes.....0.bp.",
+	    "X..predicted.genes.....300.bp.",
+	    "X..predicted.genes.....1500.bp.",
+	    "X..predicted.genes.....3000.bp."
+	    )]
+
+data$Assembly <- as.character(data$Assembly)
+
+data <- rbind(c("max", apply(data[,2:ncol(data)],2,max)), 
+		     c("min", apply(data[,2:ncol(data)],2,min)), 
+		     data)
+data[,2:ncol(data)] <- sapply(data[,2:ncol(data)], as.numeric)
+
+# Calculate maximum and minimum for positive values
+data[1, !names(data) %in% "Assembly"] <- 
+data[1, !names(data) %in% "Assembly"] * 1.1
+
+data[2, !names(data) %in% "Assembly"] <- 
+data[2, !names(data) %in% "Assembly"] * 0.9
+
+axisnames <- c(
+	       "unique", 
+	       "total", 
+	       "> 300bp", 
+	       "> 1.5kbp", 
+	       "> 3kbp")
+
+radarchart(data[,-1], pcol=cols, pfcol=dens, plty=linetype, plwd=plwd,
+	   cglwd=lwd, palcex=font, calcex=font, vlcex=font, cglcol="gray",
+	   vlabels=axisnames,title=title, oma=c(0,0,0,0))
+}
+
+######################################################################################################
+
+## Read in simulated data
+dat1 <- read.delim("/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/quast_output/quast_simDat_20151028/combined_reference/transposed_report.tsv", header=T)
+
+## Read in HF
+dat2 <- read.delim("/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/quast_output/quast_X310763260-20151028/combined_reference/transposed_report.tsv", header=T)
+
+## Read in WW vs Bio17-1
+dat3 <- read.delim("/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/quast_output/quast_A02_Microthrix_Bio17-20151208/combined_reference/transposed_report.tsv", header=T)
+
+# Initialize colours, lines and fonts
+cols <- makeTransparent("darkred", "darkblue", "darkgreen", "orange", alpha=0.75)
+dens <- makeTransparent("red", "blue", "green", "yellow", alpha=0.10)
+font=3.5
+linetype <- c(1,1,2,2)
+fsize=4.5
+linewd=3.5
+mcex=5
+
+# Generate figure
+png("/home/shaman/Documents/Publications/IMP-manuscript/figures/comparison_detailed_v5.png", 
+    width=2400, height=2000)
+
+## Bottom, left, top, right
+par(oma = c(4, 1.5, 3, 0))
+par(mfrow=c(3,3))
+plot_basic_stats(dat1, font=fsize, lwd=linewd, plwd=linewd)
+mtext("Assembly", side=3, line=2, cex=mcex, font=2)
+mtext("SM", side=2, line=1, srt=90, cex=mcex, font=4) 
+plot_misassembly_stats(dat1, font=fsize, lwd=linewd, plwd=linewd)
+mtext("Reference", side=3, line=2, cex=mcex, font=2)
+plot_gene_stats(dat1, font=fsize, lwd=linewd, plwd=linewd)
+mtext("Genes", side=3, line=2, cex=mcex, font=2)
+
+plot_basic_stats(dat2, font=fsize, lwd=linewd, plwd=linewd)
+mtext("HF", side=2, line=1, srt=90, cex=mcex, font=4) 
+plot_misassembly_stats(dat2, font=fsize, lwd=linewd, plwd=linewd)
+plot_gene_stats(dat2, font=fsize, lwd=linewd, plwd=linewd)
+
+plot_basic_stats(dat3, font=fsize, lwd=linewd, plwd=linewd)
+mtext("WW", side=2, line=0, srt=90, cex=mcex, font=4) 
+plot_misassembly_stats(dat3, font=fsize, lwd=linewd, plwd=linewd)
+plot_gene_stats(dat3, font=fsize, lwd=linewd, plwd=linewd)
+
+par(fig = c(0, 1, 0, 1), oma = c(0.5, 0, 0, 0), mar = c(0, 0, 0, 0), new = TRUE)
+plot(0, 0, type = "n", bty = "n", xaxt = "n", yaxt = "n")
+legend("bottom", legend=dat1[,1], xpd = TRUE, horiz = TRUE, inset = c(0, 
+    0), bty = "o", col = cols, lty=linetype, lwd = c(4,4,4,4), cex = 4,
+       text.font=c(4,4,4,4), box.lty=1, box.lwd=3, box.col="gray",
+       text.col=cols)
+dev.off()
+
