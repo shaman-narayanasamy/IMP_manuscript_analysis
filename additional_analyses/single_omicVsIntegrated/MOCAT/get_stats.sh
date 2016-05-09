04e81294b42877454ee89e69d8d6b65b9480b2eb
Shaman Narayanasamy <shaman.narayanasamy@uni.lu>
2016-05-09 11:44:42 +0200

    Correct commands

--- additional_analyses/single_omicVsIntegrated/MOCAT/get_stats.sh
+++ additional_analyses/single_omicVsIntegrated/MOCAT/get_stats.sh
@@ -5,6 +5,7 @@ source ../preload_modules.sh
 MGMT_ROOT="/scratch/users/snarayanasamy/IMP_MS_data/mappingSingleOmics/MOCAT/Combined"
 MG_ROOT="/scratch/users/snarayanasamy/IMP_MS_data/mappingSingleOmics/MOCAT/MG"
 MT_ROOT="/scratch/users/snarayanasamy/IMP_MS_data/mappingSingleOmics/MOCAT/MT"
+OUTDIR="/scratch/users/snarayanasamy/IMP_MS_data/mappingSingleOmics/MOCAT"
 
 declare -a SAMPLES=("SM" "HF1" "HF2" "HF3" "HF4" "HF5" "WW1" "WW2" "WW3" "WW4" "BG")
 
@@ -20,6 +21,9 @@ do
     S1="$S"
     echo "Continue"
   fi
+
+  mkdir ${OUTDIR}/${S}
+
   MG2MGMT="${MGMT_ROOT}/${S1}/$S/${S}_MG-mapping.bam"
   MT2MGMT="${MGMT_ROOT}/${S1}/$S/${S}_MT-mapping.bam"
   MG2MG="${MG_ROOT}/${S1}/${S}/${S}_MG-mapping.bam"
@@ -28,21 +32,21 @@ do
   MT2MT="${MT_ROOT}/${S1}/${S}/${S}_MT-mapping.bam"
 
   ls $MG2MGMT
-  samtools flagstat $MG2MGMT > ${MGMT_ROOT}/${S1}/$S/MG_reads-x-_MGMT-assm.flagstat.txt
+  samtools flagstat $MG2MGMT > ${OUTDIR}/$S/MG_reads-x-_MGMT-assm.flagstat.txt
 
   ls $MT2MGMT
-  samtools flagstat $MG2MGMT > ${MGMT_ROOT}/${S1}/$S/MT_reads-x-_MGMT-assm.flagstat.txt
+  samtools flagstat $MG2MGMT > ${OUTDIR}/$S/MT_reads-x-_MGMT-assm.flagstat.txt
 
   ls $MG2MG
-  samtools flagstat $MG2MGMT > ${MG_ROOT}/${S1}/$S/MG_reads-x-_MG-assm.flagstat.txt
+  samtools flagstat $MG2MG > ${OUTDIR}/$S/MG_reads-x-_MG-assm.flagstat.txt
 
   ls $MT2MG
-  samtools flagstat $MG2MGMT > ${MG_ROOT}/${S1}/$S/MT_reads-x-_MG-assm.flagstat.txt
+  samtools flagstat $MT2MG > ${OUTDIR}/$S/MT_reads-x-_MG-assm.flagstat.txt
 
   ls $MG2MT
-  samtools flagstat $MG2MGMT > ${MG_ROOT}/${S1}/$S/MG_reads-x-_MT-assm.flagstat.txt
+  samtools flagstat $MG2MT > ${OUTDIR}/$S/MG_reads-x-_MT-assm.flagstat.txt
 
   ls $MT2MT
-  samtools flagstat $MG2MGMT > ${MG_ROOT}/${S1}/$S/MT_reads-x-_MT-assm.flagstat.txt
+  samtools flagstat $MT2MT > ${OUTDIR}/$S/MT_reads-x-_MT-assm.flagstat.txt
 
 done
