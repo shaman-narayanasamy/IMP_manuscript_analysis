c44d77d391170f3f753d3436c8a8e58501f31050
Shaman Narayanasamy <shaman.narayanasamy@uni.lu>
2016-05-12 13:31:51 +0200

    Changes to figure

--- additional_analyses/comparison/comparison.R
+++ additional_analyses/comparison/comparison.R
@@ -35,7 +35,7 @@ makeTransparent = function(..., alpha=0.5) {
 
 ###############################################################################################################################
 ### FUNCTION: 5-axis radar chart
-plot.8axis <- function(dat, cols, dens, font, linetype, 
+plot.6axis <- function(dat, cols, dens, font, linetype, 
 			     fsize, linewd, mcex, lwd, plwd, title = "") {
 dat <- dat[,c("Assembly", 
        "X..contigs.....1000.bp.", 
@@ -43,14 +43,12 @@ dat <- dat[,c("Assembly",
        "X..predicted.genes..unique.",
        "X..misassemblies",
        "Genome.fraction....",
-       "MG_mapped",
        "MG_properly_paired",
-       "MT_mapped",
        "MT_properly_paired"
        )]
 
-colnames(dat) = c("Assembly", "contigs \u2265 1kb", "N50 length", "no. of genes", "misassemblies", "genome fraction (%)", 
-		  "MG mapped", "MG properly paired", "MT mapped", "MT properly paired")
+colnames(dat) = c("Assembly", "contigs \u2265 1kb", "N50 length", "no. of genes", "misassemblies", 
+		  "genome fraction (%)", "MG properly paired", "MT properly paired")
 
 dat <- rbind(
 	     c("max", apply(dat[, 2:ncol(dat)], 2, max)), 
@@ -69,14 +67,14 @@ dat[2, !names(dat) %in% c("Assembly")] <-
 dat[1, names(dat) %in% c("genome fraction (%)")] <- 100
 dat[2, names(dat) %in% c("genome fraction (%)")] <- 0
 
-dat[1, names(dat) %in% c("MG mapped")] <- 100
-dat[2, names(dat) %in% c("MG mapped")] <- 0
+#dat[1, names(dat) %in% c("MG mapped")] <- 100
+#dat[2, names(dat) %in% c("MG mapped")] <- 0
 
 dat[1, names(dat) %in% c("MG properly paired")] <- 100
 dat[2, names(dat) %in% c("MG properly paired")] <- 0
 
-dat[1, names(dat) %in% c("MT mapped")] <- 100
-dat[2, names(dat) %in% c("MT mapped")] <- 0
+#dat[1, names(dat) %in% c("MT mapped")] <- 100
+#dat[2, names(dat) %in% c("MT mapped")] <- 0
 
 dat[1, names(dat) %in% c("MT properly paired")] <- 100
 dat[2, names(dat) %in% c("MT properly paired")] <- 0
@@ -100,14 +98,12 @@ dat <- dat[,c("Assembly",
        "X..contigs.....1000.bp.", 
        "N50", 
        "X..predicted.genes..unique.",
-       "MG_mapped",
        "MG_properly_paired",
-       "MT_mapped",
        "MT_properly_paired"
        )]
 
-colnames(dat)  <- c("Assembly", "contigs \u2265 1kb", "N50 length", "no. of genes", 
-			   "MG mapped", "MG properly paired", "MT mapped", "MT properly paired")
+colnames(dat)  <- c("Assembly", "contigs \u2265 1kb", "N50 length", 
+		    "no. of genes", "MG properly paired", "MT properly paired")
 
 dat <- rbind(
 	     c("max", apply(dat[, 2:ncol(dat)], 2, max)), 
@@ -122,14 +118,14 @@ dat[1, !names(dat) %in% c("Assembly")] <-
 dat[2, !names(dat) %in% c("Assembly")] <- 
     dat[2, !names(dat) %in% c("Assembly")] * 0.75
 
-dat[1, names(dat) %in% c("MG mapped")] <- 100
-dat[2, names(dat) %in% c("MG mapped")] <- 0
+#dat[1, names(dat) %in% c("MG mapped")] <- 100
+#dat[2, names(dat) %in% c("MG mapped")] <- 0
 
 dat[1, names(dat) %in% c("MG properly paired")] <- 100
 dat[2, names(dat) %in% c("MG properly paired")] <- 0
 
-dat[1, names(dat) %in% c("MT mapped")] <- 100
-dat[2, names(dat) %in% c("MT mapped")] <- 0
+#dat[1, names(dat) %in% c("MT mapped")] <- 100
+#dat[2, names(dat) %in% c("MT mapped")] <- 0
 
 dat[1, names(dat) %in% c("MT properly paired")] <- 100
 dat[2, names(dat) %in% c("MT properly paired")] <- 0
@@ -193,57 +189,57 @@ lwd=16
 plwd=15
 
 
-pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/SM_radarChart_v4.pdf", 
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/SM_radarChart_v5.pdf", 
     width=38, height=25)
-plot.8axis(SM_quast[SM_quast$Assembly%in%assm_mgmt,], cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
+plot.6axis(SM_quast[SM_quast$Assembly%in%assm_mgmt,], cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
 dev.off()
 
-pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/HF1_radarChart_v4.pdf", 
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/HF1_radarChart_v5.pdf", 
     width=38, height=25)
 plot.5axis(HF1_quast[HF1_quast$Assembly%in%assm_mgmt,], cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
 dev.off()
 
-pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/HF2_radarChart_v4.pdf", 
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/HF2_radarChart_v5.pdf", 
     width=38, height=25)
 plot.5axis(HF2_quast[HF2_quast$Assembly%in%assm_mgmt,], cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
 dev.off()
 
-pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/HF3_radarChart_v4.pdf", 
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/HF3_radarChart_v5.pdf", 
     width=38, height=25)
 plot.5axis(HF3_quast[HF3_quast$Assembly%in%assm_mgmt,], cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
 dev.off()
 
-pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/HF4_radarChart_v4.pdf", 
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/HF4_radarChart_v5.pdf", 
     width=38, height=25)
 plot.5axis(HF4_quast[HF4_quast$Assembly%in%assm_mgmt,], cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
 dev.off()
 
-pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/HF5_radarChart_v4.pdf", 
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/HF5_radarChart_v5.pdf", 
     width=38, height=25)
 plot.5axis(HF5_quast[HF5_quast$Assembly%in%assm_mgmt,], cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
 dev.off()
 
-pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/WW1_radarChart_v4.pdf", 
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/WW1_radarChart_v5.pdf", 
     width=38, height=25)
 plot.5axis(WW1_quast[WW1_quast$Assembly%in%assm_mgmt,], cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
 dev.off()
 
-pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/WW2_radarChart_v4.pdf", 
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/WW2_radarChart_v5.pdf", 
     width=38, height=25)
 plot.5axis(WW2_quast[WW2_quast$Assembly%in%assm_mgmt,], cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
 dev.off()
 
-pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/WW3_radarChart_v4.pdf", 
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/WW3_radarChart_v5.pdf", 
     width=38, height=25)
 plot.5axis(WW3_quast[WW3_quast$Assembly%in%assm_mgmt,], cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
 dev.off()
 
-pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/WW4_radarChart_v4.pdf", 
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/WW4_radarChart_v5.pdf", 
     width=38, height=25)
 plot.5axis(WW4_quast[WW4_quast$Assembly%in%assm_mgmt,], cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
 dev.off()
 
-pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/BG_radarChart_v4.pdf", 
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/BG_radarChart_v5.pdf", 
     width=38, height=25)
 plot.5axis(BG_quast[BG_quast$Assembly%in%assm_mgmt,], cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
 dev.off()
@@ -275,18 +271,18 @@ write.table(as.data.frame(all.dat), "/home/shaman/Documents/Publications/IMP-man
 ### Prepare for data summary radar chart
 all.dat$Assembly <- as.factor(all.dat$Assembly)
 
-all.dat.sum <- aggregate(all.dat[,-c(1:2, 28:31)], by=list(all.dat$Assembly), FUN=sum)
+all.dat.sum <- aggregate(all.dat[,-c(1:2, 28:47)], by=list(all.dat$Assembly), FUN=sum)
 colnames(all.dat.sum)[1] <- "Assembly"
 
 all.dat.mean <- aggregate(all.dat[,c(28:31)], by=list(all.dat$Assembly), FUN=mean)
 colnames(all.dat.mean)[1] <- "Assembly"
 
 
-all.dat.agg <- cbind.data.frame(all.dat.sum, all.dat.mean, ref.dat)
+all.dat.agg <- cbind.data.frame(all.dat.sum, all.dat.mean, ref.dat[1:10,])
 
 ## Generate summary/cummulative radar chart
-pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/ALL_radarChart_v4.pdf", 
+pdf("/home/shaman/Documents/Publications/IMP-manuscript/figures/second_iteration/ALL_radarChart_v5.pdf", 
     width=38, height=25)
-plot.8axis(all.dat.agg[all.dat.agg$Assembly%in%assm_mgmt,], cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
+plot.6axis(all.dat.agg[all.dat.agg$Assembly%in%assm_mgmt,], cols, dens, font, linetype, fsize, linewd, mcex, lwd, plwd) 
 dev.off()
 
