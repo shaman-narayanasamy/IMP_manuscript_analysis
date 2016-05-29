defb46882f14aff2d9ba3bcbfa2983dc1a5c7313
Shaman Narayanasamy <shaman.narayanasamy@uni.lu>
2016-05-29 15:34:58 +0200

    Add new R script

---
+++ prodigal_analysis/add_data.R
@@ -0,0 +1,19 @@
+#!/bin/R
+require(plyr)
+
+
+load("/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/HMP_IGC_mapping/HMP_IGC_mapping.Rdat")
+
+prodigal.dat <- read.table("/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/prodigal_analysis/prodigal_summary.tsv", header=T)
+
+all.dat.3$Assembly <- mapvalues(all.dat.3$Assembly, from = c("MetAmos_MG", "MetAmos_MGMT"),
+				to = c("MetAMOS_MG", "MetAMOS_MGMT")
+)
+
+all.dat.4 <- merge(all.dat.3, prodigal.dat, by=c("Dataset", "Assembly"), all.y=F)
+
+all.dat.4 <- all.dat.4[-which(all.dat.4$Assembly=="IMP_MT"),]
+
+write.table(all.dat.4, "/home/shaman/Documents/Publications/IMP-manuscript/tables/second_iteration/all_comparison-v4.tsv", sep="\t", quote=F, row.names=F)
+
+#save.image("/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/prodigal_analysis/prodigal_analyses.Rdat")
