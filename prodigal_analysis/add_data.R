6c4b2490b7ba5e112124e29c91357c32a1f19e36
Shaman Narayanasamy <shaman.narayanasamy@uni.lu>
2016-06-02 13:24:26 +0200

    Generate new table

--- prodigal_analysis/add_data.R
+++ prodigal_analysis/add_data.R
@@ -1,19 +1,20 @@
 #!/bin/R
 require(plyr)
 
-
 load("/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/HMP_IGC_mapping/HMP_IGC_mapping.Rdat")
 
 prodigal.dat <- read.table("/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/prodigal_analysis/prodigal_summary.tsv", header=T)
 
-all.dat.3$Assembly <- mapvalues(all.dat.3$Assembly, from = c("MetAmos_MG", "MetAmos_MGMT"),
-				to = c("MetAMOS_MG", "MetAMOS_MGMT")
+all.dat.3$Assembly <- mapvalues(all.dat.3$Assembly, from = c("MetAmos_MG", "MetAmos_MGMT", "IMP_IGC"),
+				to = c("MetAMOS_MG", "MetAMOS_MGMT", "IGC_mapping")
 )
 
-all.dat.4 <- merge(all.dat.3, prodigal.dat, by=c("Dataset", "Assembly"), all.y=F)
+all.dat.4 <- merge(all.dat.3, prodigal.dat, by=c("Dataset", "Assembly"), all=T)
 
-all.dat.4 <- all.dat.4[-which(all.dat.4$Assembly=="IMP_MT"),]
+all.dat.4 <- all.dat.4[-which(all.dat.4$Assembly%in%c("MetAMOS_MT", "MOCAT_MT", 
+						      "MetAmos_IGC", "MOCAT_IGC")),]
 
-write.table(all.dat.4, "/home/shaman/Documents/Publications/IMP-manuscript/tables/second_iteration/all_comparison-v4.tsv", sep="\t", quote=F, row.names=F)
+
+write.table(all.dat.4, "/home/shaman/Documents/Publications/IMP-manuscript/tables/second_iteration/all_comparison-v5.tsv", sep="\t", quote=F, row.names=F)
 
 #save.image("/home/shaman/Work/Data/integrated-omics-pipeline/MS_analysis/prodigal_analysis/prodigal_analyses.Rdat")
