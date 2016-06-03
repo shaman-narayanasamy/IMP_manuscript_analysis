f224ad47a90ae2e4db85832ca9513e34d8013e26
Shaman Narayanasamy <shaman.narayanasamy@uni.lu>
2016-06-03 10:27:40 +0200

    Correction

--- additional_analyses/HMP_gene_catalog/count_IMP_HMP.sh
+++ additional_analyses/HMP_gene_catalog/count_IMP_HMP.sh
@@ -16,12 +16,12 @@ IMP_MT_BAM="/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/${S}/Assembly/
 IMP_MT_MEGAHIT_BAM="/scratch/users/snarayanasamy/IMP_MS_data/IMP_analysis/${S}_megahit/Assembly/MT.reads.sorted.bam"
 IGC_MT_BAM="/scratch/users/snarayanasamy/IMP_MS_data/HMP_IGC_mapping/${S}/MT_IMP_x_HMP.sorted.bam"
 
-paste <(echo -e "${S}_MG_IMP_IGC\t") <(sort <(samtools view -f 2 -@ ${THREADS} ${IMP_MG_BAM} | cut -f1) <(samtools view -f 2 -@ ${THREADS} ${IGC_MG_BAM} | cut -f1) | uniq | wc -l) >> IMP2IGC_mapping.txt
+paste <(echo -e "${S}_MG_IMP_IGC") <(sort <(samtools view -f 2 -@ ${THREADS} ${IMP_MG_BAM} | cut -f1) <(samtools view -f 2 -@ ${THREADS} ${IGC_MG_BAM} | cut -f1) | uniq | wc -l) > IMP2IGC_mapping.txt
 
-paste <(echo -e "${S}_MG_IMP-megahit_IGC\t") <(sort <(samtools view -f 2 -@ ${THREADS} ${IMP_MG_MEGAHIT_BAM} | cut -f1) <(samtools view -f 2 -@ ${THREADS} ${IGC_MG_BAM} | cut -f1) | uniq | wc -l) >> IMP2IGC_mapping.txt
+paste <(echo -e "${S}_MG_IMP-megahit_IGC") <(sort <(samtools view -f 2 -@ ${THREADS} ${IMP_MG_MEGAHIT_BAM} | cut -f1) <(samtools view -f 2 -@ ${THREADS} ${IGC_MG_BAM} | cut -f1) | uniq | wc -l) >> IMP2IGC_mapping.txt
 
-<(echo -e "${S}_MT_IMP_IGC\t") <(sort <(samtools view -f 2 -@ ${THREADS} ${IMP_MT_BAM} | cut -f1) <(samtools view -f 2 -@ ${THREADS} ${IGC_MT_BAM} | cut -f1) | uniq | wc -l) >> IMP2IGC_mapping.txt
+paste <(echo -e "${S}_MT_IMP_IGC") <(sort <(samtools view -f 2 -@ ${THREADS} ${IMP_MT_BAM} | cut -f1) <(samtools view -f 2 -@ ${THREADS} ${IGC_MT_BAM} | cut -f1) | uniq | wc -l) >> IMP2IGC_mapping.txt
 
-paste <(echo -e "${S}_MT_IMP-megahit_IGC\t") <(sort <(samtools view -f 2 -@ ${THREADS} ${IMP_MT_MEGAHIT_BAM} | cut -f1) <(samtools view -f 2 -@ ${THREADS} ${IGC_MT_BAM} | cut -f1) | uniq | wc -l) >> IMP2IGC_mapping.txt
+paste <(echo -e "${S}_MT_IMP-megahit_IGC") <(sort <(samtools view -f 2 -@ ${THREADS} ${IMP_MT_MEGAHIT_BAM} | cut -f1) <(samtools view -f 2 -@ ${THREADS} ${IGC_MT_BAM} | cut -f1) | uniq | wc -l) >> IMP2IGC_mapping.txt
 done
 date
