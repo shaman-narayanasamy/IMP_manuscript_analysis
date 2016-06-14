f0a6601e72ff08d9a90bc2734b6be413003bc4c9
Shaman Narayanasamy <shaman.narayanasamy@uni.lu>
2016-06-14 13:43:59 +0200

    Add MOCAT scipts and config files

---
+++ MOCAT_analysis/MT/biogas_plant/runALL.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#oarsub -lcpu=1,walltime=72:00:00  -p "memcpu='258538'" -n mocatLux ./run.sh
+
+samples=samples
+log=$samples.log
+
+source $HOME/.profile
+source $HOME/.bashrc
+
+export PERL5LIB=$PERL5LIB:/work/projects/ecosystem_biology/local_tools/MOCAT/src
+PATH=$PATH:/work/projects/ecosystem_biology/local_tools/MOCAT/src
+export PATH
+
+#MOCAT.pl -sf $samples -rtf -cpus 8 >$log 2>>$log 
+
+#MOCAT.pl -sf $samples -s hg19 -cpus 8 >>$log 2>>$log
+#MOCAT.pl -sf $samples -a -r hg19 -cpus 8 >>$log 2>>$log
+#MOCAT.pl -sf $samples -ar -cpus 8 -r hg19 >>$log 2>>$log
+#MOCAT.pl -sf $samples -gp assembly.revised -cpus 8 -r hg19 >>$log 2>>$log
+#MOCAT.pl -sf $samples -ss -cpus 8  >>$log 2>>$log
+
+MOCAT.pl -sf $samples -a -cpus 8 >>$log 2>>$log
+MOCAT.pl -sf $samples -ar -cpus 8  >>$log 2>>$log
+MOCAT.pl -sf $samples -gp assembly.revised -cpus 8  >>$log 2>>$log
+MOCAT.pl -sf $samples -ss -cpus 8  >>$log 2>>$log
+
